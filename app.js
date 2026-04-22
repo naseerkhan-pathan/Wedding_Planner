985b8da38996789e831783c61aed4d812d6e2724
Lovely-Naseer <nrk123786@gmail.com>
2026-04-22 17:02:35 +0530

    Updated code

--- app.js
+++ app.js
@@ -341,9 +341,8 @@ app.get('/logout', (req, res) => {
 });
 
 /* ---------------- Server ---------------- */
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
